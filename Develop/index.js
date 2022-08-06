e54ea2cf091fdc07f44034216299db1d78ea8ab2
Grayson Harvey <graycodes.nu@gmail.com>
2022-08-06 11:26:30 -0500

    readme file function

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,33 @@
 // TODO: Include packages needed for this application
+const inquirer = requirer('inquirer');
+const fs = requirer('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'introduction',
+        message: 'Introduction',
+    },
+    {
+        type: 'input',
+        name: 'goals',
+        message: 'Goals',
+    },
+    {
+        type: 'input',
+        name: 'results',
+        message: 'Results',
+    },
+];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(filename, data) {
+    fs.writeFile(README, JSON.stringify(data, null), (err) =>
+    err ? console.log(error) : console.log(data)
+    );
+};
 
 // TODO: Create a function to initialize app
 function init() {}
